acaab321f7ddff4e5089a82d9c2c18e9b6cdb0bb
Carolyne1063 <carolmartoz763@gmail.com>
2024-11-01 17:11:00 -0700

    tailwind practices

---
+++ frontend/my-project/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { PracticalsComponent } from './components/practicals/practicals.component';
+
+export const routes: Routes = [
+    { path: 'prac', component:PracticalsComponent }
+];
---
+++ frontend/my-project/src/app/components/practicals/practicals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practicals',
+  standalone: true,
+  imports: [],
+  templateUrl: './practicals.component.html',
+  styleUrl: './practicals.component.css'
+})
+export class PracticalsComponent {
+
+}
